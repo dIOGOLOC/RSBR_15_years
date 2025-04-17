05c61fad7eb6b815c15492ef524a6c84ff8345b6
dIOGO <locdiogo@gmail.com>
2025-04-16 21:25:17 -0300

    New changes.

--- docs/conf.py
+++ docs/conf.py
@@ -36,5 +36,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['docs/source/_static']
-
+html_static_path = ['source/_static']
